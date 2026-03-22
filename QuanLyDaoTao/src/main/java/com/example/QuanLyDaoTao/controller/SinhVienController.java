34f0c6bceebabd2ac6ba69f21584a2f03d3d3be5
Duy Phuc <duyphuc200536@gmail.com>
2026-03-23 03:26:50 +0700

    ver1

---
+++ QuanLyDaoTao/src/main/java/com/example/QuanLyDaoTao/controller/SinhVienController.java
@@ -0,0 +1,34 @@
+package com.example.QuanLyDaoTao.controller;
+
+import com.example.QuanLyDaoTao.entity.SinhVien;
+import com.example.QuanLyDaoTao.repository.SinhVienRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/sinhvien")
+@CrossOrigin
+public class SinhVienController {
+
+    @Autowired
+    private SinhVienRepository sinhVienRepository;
+
+
+    @GetMapping("/{maSV}")
+    public SinhVien getThongTinSinhVien(@PathVariable String maSV) {
+
+        return sinhVienRepository.findById(maSV).orElse(null);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody com.example.QuanLyDaoTao.dto.LoginRequest request) {
+
+        SinhVien sv = sinhVienRepository.findByMaSVAndSoDienThoai(request.getUsername(), request.getPassword());
+
+        if (sv != null) {
+            return "SUCCESS";
+        } else {
+            return "FAIL";
+        }
+    }
+}
---
+++ QuanLyDaoTao/src/main/java/com/example/QuanLyDaoTao/entity/SinhVien.java
@@ -0,0 +1,32 @@
+package com.example.QuanLyDaoTao.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "sinh_vien")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SinhVien {
+
+    @Id
+    @Column(name = "ma_sv", length = 20)
+    private String maSV;
+
+    @Column(name = "ho_ten", columnDefinition = "nvarchar(100)")
+    private String hoTen;
+
+    @Column(name = "ngay_sinh")
+    private LocalDate ngaySinh;
+
+    @Column(name = "gioi_tinh", length = 10)
+    private String gioiTinh;
+
+    @Column(name = "noi_sinh", columnDefinition = "nvarchar(100)")
+    private String noiSinh;
+
+    @Column(name = "so_dien_thoai", length = 15)
+    private String soDienThoai;
+}
